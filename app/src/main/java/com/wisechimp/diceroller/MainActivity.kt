0a6117ff47c0148906560409d124b40477adb076
Stuart Hicks <stuart.hicks@gmail.com>
2019-08-16 22:34:00 +0200

    Added dice images and wiring up to button

--- app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
+++ app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wisechimp.diceroller
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
@@ -16,7 +17,15 @@ class MainActivity : AppCompatActivity() {
 
     fun rollDice(view: View) {
         val randomInt = Random.nextInt(6) +1
-        dice_roll_result_text_view.text = randomInt.toString()
-        Toast.makeText(this, "Rolled 'em", Toast.LENGTH_SHORT).show()
+        val diceRollResult = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val rollingDice: ImageView = findViewById(R.id.dice_roll_result_image_view)
+        rollingDice.setImageResource(diceRollResult)
     }
 }
