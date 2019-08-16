5efefa6e9816185e6034c796a664d3b01ff229c9
shicks1080 <stuart.hicks@gmail.com>
2019-08-16 14:31:48 +0200

    Function added to Roll Dice button

--- app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
+++ app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.wisechimp.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun rollDice(view: View) {
+        Toast.makeText(this, "Rolled 'em", Toast.LENGTH_SHORT).show()
     }
 }
