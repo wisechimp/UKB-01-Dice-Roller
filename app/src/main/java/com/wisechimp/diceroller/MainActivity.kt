4d1eecc530581760e8ddf77c60d2e4d703486f67
shicks1080 <stuart.hicks@gmail.com>
2019-08-16 14:43:21 +0200

    Dice roll generates random number

--- app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
+++ app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
     fun rollDice(view: View) {
+        val randomInt = Random.nextInt(6) +1
+        dice_roll_result_text_view.text = randomInt.toString()
         Toast.makeText(this, "Rolled 'em", Toast.LENGTH_SHORT).show()
     }
 }
