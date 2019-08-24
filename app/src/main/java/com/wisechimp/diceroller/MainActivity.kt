9ef51d2dff76e94e6b396b8c09957300c1582c9c
Stuart Hicks <stuart.hicks@gmail.com>
2019-08-24 20:41:58 +0200

    View Model Added
    The previous commit represents the end of the instructions for this app. Here I take it further by moving the code for generating the dice to a View Model.

--- app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
+++ app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
@@ -1,31 +1,28 @@
 package com.wisechimp.diceroller
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceViewModel:MainActivityViewModel
+    private lateinit var rollingDice:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceViewModel = ViewModelProviders.of(this)[MainActivityViewModel::class.java]
+        rollingDice = findViewById(R.id.dice_roll_result_image_view)
+        rollingDice.setImageResource(diceViewModel.diceRollValue)
     }
 
     fun rollDice(view: View) {
-        val randomInt = Random.nextInt(6) +1
-        val diceRollResult = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        val rollingDice: ImageView = findViewById(R.id.dice_roll_result_image_view)
+        val diceRollResult = diceViewModel.diceRollAttempt()
+        Log.i("Roll result", diceRollResult.toString())
         rollingDice.setImageResource(diceRollResult)
     }
 }
---
+++ app/src/main/java/com/wisechimp/diceroller/MainActivityViewModel.kt
@@ -0,0 +1,23 @@
+package com.wisechimp.diceroller
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import kotlin.random.Random
+
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+    var diceRollValue = R.drawable.dice_1
+
+    fun diceRollAttempt (): Int {
+        val randomInt = Random.nextInt(6) + 1
+        diceRollValue = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return diceRollValue
+    }
+}
