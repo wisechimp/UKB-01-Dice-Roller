38fb0acf216c1e6df1ee95eb549caf59c84a3c53
shicks1080 <stuart.hicks@gmail.com>
2019-08-26 11:35:48 +0200

    Refactor to clean the code
    Use Peter Somerhoff's clean code principals to remove the lateinit (which in itself is better than having null variables) plus findviewbyid and simply take advantage of the kotlin-android-extentions to recognise the view's id.

--- app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
+++ app/src/main/java/com/wisechimp/diceroller/MainActivity.kt
@@ -5,24 +5,22 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var diceViewModel:MainActivityViewModel
-    private lateinit var rollingDice:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         diceViewModel = ViewModelProviders.of(this)[MainActivityViewModel::class.java]
-        rollingDice = findViewById(R.id.dice_roll_result_image_view)
-        rollingDice.setImageResource(diceViewModel.diceRollValue)
+        dice_roll_result_image_view.setImageResource(diceViewModel.diceRollValue)
     }
 
     fun rollDice(view: View) {
         val diceRollResult = diceViewModel.diceRollAttempt()
         Log.i("Roll result", diceRollResult.toString())
-        rollingDice.setImageResource(diceRollResult)
+        dice_roll_result_image_view.setImageResource(diceRollResult)
     }
 }
